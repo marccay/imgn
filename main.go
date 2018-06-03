bd42df14e498d1ea36e6f69a8e780187d258685a
marccay <info@marccayanong.com>
2018-06-03 02:48:04 -0400

    added initial code for bunch files
    single folder with multiple images with concurrency added.

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sync"
 )
+
+var bunchFiles sync.WaitGroup
 
 func main() {
 	args := getArgs()
@@ -16,10 +19,42 @@ func main() {
 		os.Exit(1)
 	}
 
-	single(path, all)
+	stat, err := os.Stat(path)
+	if err != nil {
+		fmt.Println("trouble accessing path info")
+		os.Exit(1)
+	}
+
+	if stat.IsDir() {
+		var bunchFilepaths []string
+		filepath.Walk(path, func(fp string, info os.FileInfo, err error) error {
+			if err != nil {
+				fmt.Println("failed to access file path")
+				os.Exit(2)
+			}
+			if info.IsDir() {
+				return nil
+			}
+			bunchFilepaths = append(bunchFilepaths, fp)
+			return nil
+		})
+		if err != nil {
+			fmt.Println("error walking the path")
+			os.Exit(2)
+		}
+		bunchFiles.Add(len(bunchFilepaths))
+		for _, f := range bunchFilepaths {
+			go single(f, all)
+		}
+		bunchFiles.Wait()
+	} else {
+		single(path, all)
+	}
+
 }
 
 func single(file string, all multipleGroups) {
+	defer bunchFiles.Done()
 	img := openImage(file)
 	fmt.Println("image opened")
 	pixels := readPixels(img)
